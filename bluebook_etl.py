5ef31b956a9ef7ac19a1c62ac47e87e5f7737263
Felipe Silverio <felipe.sql@outlook.com>
2025-04-22 15:06:11 +0100

    First upload

---
+++ bluebook_etl.py
@@ -0,0 +1,121 @@
+import pandas as pd
+import glob
+import numpy as np
+import re
+
+class ETL_Bluebook():
+
+    def __init__(self, conn, cursor):
+
+        self.conn = conn
+        self.cursor = cursor
+
+        self.cols = ['summary_date','summary_netsales_total', 'summary_tktcount_total', 'summary_tktcount_ontime', 'tktcount_ontime_pct',
+                     'tkt_avg','av_time_all','av_time_callin','av_time_counter','av_time_stall','av_time_dt','av_time_patio',
+                     'av_time_oa','av_time_delivery','reply_time','cash_due','cash_deposits', 'summary_overshort_amount',
+                     'cancel_tkt','cancel_tkt_cnt','crew_labor','total_labor', 'food_cost','food_cost_pct']
+
+        self.cols_to_update=['stores_number','summary_date','summary_netsales_total', 
+                             'summary_tktcount_total', 'summary_tktcount_ontime','summary_overshort_amount']
+        
+        self.df = []
+    
+    @staticmethod
+    def is_date(value):
+        if isinstance(value, str):
+            return bool(re.match(r'^\d{2}-\d{2}-\d{2}$', value))
+        return False  # safely handle None or other types
+
+    def extract(self):
+        print('Starting extract')
+        # df = pd.read_excel('/Users/felipesilverio/Documents/GitHub/miscellanious/BlueBook_OSonic_TEMP.xlsx', header=4)
+        excel_files = glob.glob('./tmp/downloaded_files/*.xlsx')
+        df = pd.read_excel(excel_files[0], header=4)
+
+        df.columns = self.cols
+        df.drop(0, inplace=True)
+        self.df = df.copy()
+        print('Finished extract')
+
+
+    def transform(self):
+        print('Starting transformations')
+        df = self.df.copy()
+        # Apply function to create new column
+        df['is_date'] = df['summary_date'].apply(self.is_date).astype(int)
+        df['summary_date'] = df['summary_date'].apply(lambda x: x if self.is_date(x) else str(x)[:4])
+
+        df['stores_number'] = df.summary_date
+        for idx in df.index:
+            val = df.loc[idx, 'summary_date']
+            if val.isdigit():  # If it's a numeric string (like 1082 or 1130)
+                # Fill this value for the next 7 rows (including itself if needed)
+                df.loc[idx:idx+7, 'stores_number'] = val
+        df = df[df.is_date==1].copy()
+
+
+        df['summary_date'] = pd.to_datetime(df['summary_date'], format='%m-%d-%y', errors='coerce')
+
+        df.reset_index(drop=True)
+        self.df = df[self.cols_to_update]
+        print('Finished transformations')
+
+    def load(self):
+        print('Starting load')
+
+        df = self.df.copy()
+
+        # df = pd.read_csv('/Users/felipesilverio/Documents/GitHub/miscellanious/Consolidate_BB_v2.csv')
+        df = df.dropna(axis=1, how='all')
+
+        # Step 1: Create temporary table
+
+        self.cursor.execute("""
+            CREATE TABLE CMGSOAR.update_data (
+                stores_number VARCHAR(28),
+                summary_date VARCHAR(28),
+                summary_netsales_total VARCHAR(28),
+                summary_tktcount_total VARCHAR(28),
+                summary_tktcount_ontime VARCHAR(28),
+                summary_overshort_amount VARCHAR(28)
+        );
+        """)
+
+        # self.df.to_csv('consolidate_bb.csv')
+
+        # Step 2: Load data into temporary table
+        values = df.values.tolist()
+
+        values = [[None if isinstance(val, float) and np.isnan(val) else val for val in row] for row in values]
+        columns2 = ', '.join(df.columns)
+        placeholders = ', '.join(['%s'] * len(df.columns))
+
+        sql = f"""INSERT INTO CMGSOAR.update_data ({columns2}) VALUES ({placeholders})"""
+        self.cursor.executemany(sql, values)
+
+        self.cursor.execute("""
+            UPDATE CMGSOAR.store_summary AS target
+            JOIN CMGSOAR.update_data AS upd
+            ON target.stores_number = upd.stores_number
+            AND target.summary_date = upd.summary_date
+            SET 
+                target.summary_netsales_total = upd.summary_netsales_total,
+                target.summary_tktcount_total = upd.summary_tktcount_total,
+                target.summary_tktcount_ontime = upd.summary_tktcount_ontime,
+                target.summary_overshort_amount = upd.summary_overshort_amount;
+        """)
+
+        self.cursor.execute("""
+            DROP TABLE CMGSOAR.update_data;
+        """)
+
+        # Commit and close
+        self.conn.commit()
+        self.cursor.close()
+        self.conn.close()
+        print('Finishing load')
+
+    def run(self):
+        self.extract()
+        self.transform()
+        self.load()
---
+++ main.py
@@ -0,0 +1,80 @@
+import pandas as pd
+import mysql.connector
+import env
+import numpy as np
+import re
+import time
+import os
+from sharepointetl import load_env_vars, get_access_token, get_site_id, download_directory, download_file, delete_files_in_directory
+import pandas as pd
+import warnings
+from datetime import datetime, timedelta
+import env
+import requests
+import glob
+from bluebook_etl import ETL_Bluebook
+
+def run_bluebook_menu():
+    # ------------- Loadings vars
+    env_vars = load_env_vars()
+    access_token = get_access_token(env_vars)
+    site_id = get_site_id(access_token, env_vars["site_name"])
+    run_func = ETL_Bluebook
+
+    base_path = env_vars['base_path']
+        
+    if datetime.now().month <10:
+        bluebook_folder = f"{datetime.now().year}/0{datetime.now().month}/{datetime.now().day-1}"
+    else:
+        bluebook_folder = f"{datetime.now().year}/{datetime.now().month}/{datetime.now().day-1}"
+        
+    bluebook_path = os.path.join(base_path, bluebook_folder)
+    bluebook_path = os.path.normpath(bluebook_path)
+
+    directory_path = remote_path = bluebook_path
+
+    # ----------------- DOWNLOADING
+    print('Starting Function')
+    local_directory = env_vars['local_directory']
+    print(f'Loaded directory {local_directory}')
+
+    downloaded = False
+    try:
+        print('Starting download')
+        download_directory(access_token, site_id, remote_path, local_directory)
+        print('Downloaded')
+        downloaded = True
+    
+    except Exception as e:
+        print(f"Download error for {run_func.__name__}: {e}")
+
+    if downloaded:
+        # ------------ running ETL
+        try:
+            print('Running main function')
+
+            conn = mysql.connector.connect(
+                user=env_vars.DBUSER,
+                password=env_vars.PASSWORD,
+                host=env_vars.HOST,
+                database=env_vars.DBNAME
+            )
+
+            cursor = conn.cursor()
+
+            run_func(conn, cursor).run()
+
+        except Exception as e:
+            print(f"Problem running {run_func.__name__}: {e}")
+    else:
+        print(f"Skipping {run_func.__name__} due to download failure.")
+
+    # removing the files from the folders locally
+
+    if os.path.exists(env_vars['local_directory']):
+        delete_files_in_directory(env_vars['local_directory'])
+        print(f"Directory {env_vars['local_directory']} deleted")
+    else:
+        print(f"Directory {env_vars['local_directory']} does not exist to delete files.")
+
+    
